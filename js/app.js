d6f141191b8d26d31f63926cfd4ff1cc4c19a6df
Tilok Paul <tilokpaul.web@gmail.com>
2022-03-11 15:25:25 +0600

    About JS question display only HOME Tab

--- js/app.js
+++ js/app.js
@@ -35,16 +35,19 @@ const switchTab = (id) => {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById( "about-js" ).style.display = "block";
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById( "about-js" ).style.display = "none";
 
         displayLikedPosts();
     } else {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
+        document.getElementById( "about-js" ).style.display = "none";
         
        displayReportedPosts();
     }
@@ -156,7 +159,6 @@ const displayReportedPosts = () => {
     const reportedAarticale = clearOldData('reported-articale')
     const reportedPosts = getReportedPosts();
     const nodeLIst = document.querySelectorAll('article')
-    console.log(nodeLIst)
     reportedPosts.forEach((post) => {
         const div = createPost(post);
         reportedAarticale.appendChild(div);
